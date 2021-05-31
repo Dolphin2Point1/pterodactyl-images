484ac03bd318b68649d07ba1f1ef871674f75646
Dolphin2Point1 <finb7729@gmail.com>
2021-05-31 12:50:31 -0500

    i need to debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cd /home/container
 
+# debug
+ls
+find .
+
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
